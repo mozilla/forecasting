e962b63b933170128e8b91cba7c0d2884b026573
Jeff Klukas <jeff@klukas.net>
2019-12-10 16:07:21 -0500

    Append MAU suffix to all datasource names

--- simpleprophet/simpleprophet/pipeline.py
+++ simpleprophet/simpleprophet/pipeline.py
@@ -17,20 +17,20 @@ from simpleprophet.utils import get_latest_date
 
 
 FIRST_MODEL_DATES = {
-      'Fennec iOS': pd.to_datetime("2019-03-08").date(),
-      'fxa_global': pd.to_datetime("2019-03-08").date(),
-      'Firefox Lite': pd.to_datetime("2019-05-20").date(),
-      'Focus iOS': pd.to_datetime("2019-03-08").date(),
-      'Fenix': pd.to_datetime("2019-07-05").date(),
-      'desktop_global': pd.to_datetime("2019-03-08").date(),
-      'FirefoxConnect': pd.to_datetime("2019-03-08").date(),
-      'FirefoxForFireTV': pd.to_datetime("2019-03-08").date(),
-      'Fennec Android': pd.to_datetime("2019-03-08").date(),
-      'desktop_tier1': pd.to_datetime("2019-03-08").date(),
-      'fxa_tier1': pd.to_datetime("2019-03-08").date(),
-      'Focus Android': pd.to_datetime("2019-03-08").date(),
-      'mobile_global': pd.to_datetime("2019-03-08").date(),
-      'mobile_tier1': pd.to_datetime("2019-03-08").date(),
+      'Fennec iOS MAU': pd.to_datetime("2019-03-08").date(),
+      'fxa_global_mau': pd.to_datetime("2019-03-08").date(),
+      'Firefox Lite MAU': pd.to_datetime("2019-05-20").date(),
+      'Focus iOS MAU': pd.to_datetime("2019-03-08").date(),
+      'Fenix MAU': pd.to_datetime("2019-07-05").date(),
+      'desktop_global_mau': pd.to_datetime("2019-03-08").date(),
+      'FirefoxConnect MAU': pd.to_datetime("2019-03-08").date(),
+      'FirefoxForFireTV MAU': pd.to_datetime("2019-03-08").date(),
+      'Fennec Android MAU': pd.to_datetime("2019-03-08").date(),
+      'desktop_tier1_mau': pd.to_datetime("2019-03-08").date(),
+      'fxa_tier1_mau': pd.to_datetime("2019-03-08").date(),
+      'Focus Android MAU': pd.to_datetime("2019-03-08").date(),
+      'mobile_global_mau': pd.to_datetime("2019-03-08").date(),
+      'mobile_tier1_mau': pd.to_datetime("2019-03-08").date(),
 }
 FORECAST_HORIZON = pd.to_datetime("2020-12-31").date()
 DEFAULT_BQ_PROJECT = "moz-fx-data-derived-datasets"
