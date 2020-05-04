a77360004337829b5328670394344286f413fcf8
Anna Scholtz <anna@scholtzan.net>
2020-05-04 15:40:06 -0700

    Refactor queries

--- anomdtct/anomdtct/data.py
+++ anomdtct/anomdtct/data.py
@@ -10,223 +10,7 @@ import pickle
 # Note we obtain the most recent geo for each profile, as updates to the IPGeo
 # database that we use create excessive noise.  Also note that we restrict to
 # the top 1000 cities according to DAU on an arbitrary day.
-RAW_DATA_QUERIES = {
-    "light_funnel_dau_city": '''
-        WITH top_cities_t AS (
-          SELECT
-            COUNT(client_id) AS dau,
-            CONCAT(
-              country, ":",
-              geo_subdivision1, ":",
-              geo_subdivision2, ":",
-              city
-            ) AS geo
-          FROM `moz-fx-data-shared-prod.telemetry.clients_daily`
-          WHERE
-            submission_date = "2020-03-01"
-            AND city != "??"
-          GROUP BY
-            country,
-            geo_subdivision1,
-            geo_subdivision2,
-            city
-          ORDER BY dau DESC
-          LIMIT 1000
-        ),
-        geo_t AS (
-          SELECT
-            client_id,
-            geo
-          FROM (
-            SELECT
-              client_id,
-              CONCAT(
-                country, ":",
-                geo_subdivision1, ":",
-                geo_subdivision2, ":",
-                cd_t.city
-              ) AS geo,
-              ROW_NUMBER() OVER(
-                PARTITION BY client_id ORDER BY submission_date DESC
-              ) AS rn
-            FROM `moz-fx-data-shared-prod.telemetry.clients_daily` AS cd_t
-            INNER JOIN (SELECT geo FROM top_cities_t) AS top_cities_t
-            ON CONCAT(
-                country, ":",
-                geo_subdivision1, ":",
-                geo_subdivision2, ":",
-                city
-              ) = top_cities_t.geo
-            WHERE
-              submission_date > "1900-01-01"
-          )
-          WHERE rn = 1
-        )
-        SELECT
-          submission_date AS date,
-          COUNT(client_id) AS value,
-          COUNT(client_id) AS dau,
-          geo_t.geo AS geo
-        FROM (
-            SELECT
-              client_id,
-              submission_date,
-            FROM `moz-fx-data-shared-prod.telemetry.clients_daily`
-            WHERE
-              submission_date > "1900-01-01"
-              AND attribution.content IS NOT NULL
-          )
-        INNER JOIN geo_t
-        USING(client_id)
-        GROUP BY geo, submission_date
-        ''',
-    "light_funnel_dau_country": '''
-        WITH geo_t AS (
-          SELECT
-            client_id,
-            geo
-          FROM (
-            SELECT
-              client_id,
-              country AS geo,
-              ROW_NUMBER() OVER(
-                PARTITION BY client_id ORDER BY submission_date DESC
-              ) AS rn
-            FROM `moz-fx-data-shared-prod.telemetry.clients_daily` AS cd_t
-            WHERE
-              submission_date > "1900-01-01"
-          )
-          WHERE rn = 1
-        )
-        SELECT
-          submission_date AS date,
-          COUNT(client_id) AS value,
-          COUNT(client_id) AS dau,
-          geo_t.geo AS geo
-        FROM (
-            SELECT
-              client_id,
-              submission_date,
-            FROM `moz-fx-data-shared-prod.telemetry.clients_daily`
-            WHERE
-              submission_date > "1900-01-01"
-              AND attribution.content IS NOT NULL
-          )
-        INNER JOIN geo_t
-        USING(client_id)
-        GROUP BY geo, submission_date
-        ''',
-    "light_funnel_mean_active_hours_per_profile_city": '''
-        WITH top_cities_t AS (
-          SELECT
-            COUNT(client_id) AS dau,
-            CONCAT(
-              country, ":",
-              geo_subdivision1, ":",
-              geo_subdivision2, ":",
-              city
-            ) AS geo
-          FROM `moz-fx-data-shared-prod.telemetry.clients_daily`
-          WHERE
-            submission_date = "2020-03-01"
-            AND city != "??"
-          GROUP BY
-            country,
-            geo_subdivision1,
-            geo_subdivision2,
-            city
-          ORDER BY dau DESC
-          LIMIT 1000
-        ),
-        geo_t AS (
-          SELECT
-            client_id,
-            geo
-          FROM (
-            SELECT
-              client_id,
-              CONCAT(
-                country, ":",
-                geo_subdivision1, ":",
-                geo_subdivision2, ":",
-                cd_t.city
-              ) AS geo,
-              ROW_NUMBER() OVER(
-                PARTITION BY client_id ORDER BY submission_date DESC
-              ) AS rn
-            FROM `moz-fx-data-shared-prod.telemetry.clients_daily` AS cd_t
-            INNER JOIN (SELECT geo FROM top_cities_t) AS top_cities_t
-            ON CONCAT(
-                country, ":",
-                geo_subdivision1, ":",
-                geo_subdivision2, ":",
-                city
-              ) = top_cities_t.geo
-            WHERE
-              submission_date > "1900-01-01"
-          )
-          WHERE rn = 1
-        )
-        SELECT
-          submission_date AS date,
-          AVG(active_hours_sum) AS value,
-          COUNT(client_id) AS dau,
-          geo_t.geo AS geo
-        FROM (
-            SELECT
-              active_hours_sum,
-              client_id,
-              submission_date,
-            FROM `moz-fx-data-shared-prod.telemetry.clients_daily`
-            WHERE
-              submission_date > "1900-01-01"
-              AND attribution.content IS NOT NULL
-          )
-        INNER JOIN geo_t
-        USING(client_id)
-        GROUP BY geo, submission_date
-        ''',
-    "light_funnel_mean_active_hours_per_profile_country": '''
-        WITH geo_t AS (
-          SELECT
-            client_id,
-            geo
-          FROM (
-            SELECT
-              client_id,
-              country AS geo,
-              ROW_NUMBER() OVER(
-                PARTITION BY client_id ORDER BY submission_date DESC
-              ) AS rn
-            FROM `moz-fx-data-shared-prod.telemetry.clients_daily` AS cd_t
-            WHERE
-              submission_date > "1900-01-01"
-          )
-          WHERE rn = 1
-        )
-        SELECT
-          submission_date AS date,
-          AVG(active_hours_sum) AS value,
-          COUNT(client_id) AS dau,
-          geo_t.geo AS geo
-        FROM (
-            SELECT
-              active_hours_sum,
-              client_id,
-              submission_date,
-            FROM `moz-fx-data-shared-prod.telemetry.clients_daily`
-            WHERE
-              submission_date > "1900-01-01"
-              AND attribution.content IS NOT NULL
-          )
-        INNER JOIN geo_t
-        USING(client_id)
-        GROUP BY geo, submission_date
-        '''
-}
-
-
-DAILY_DATA_QUERIES = {
+QUERIES = {
     "light_funnel_dau_city": '''
         WITH top_cities_t AS (
           SELECT
@@ -283,7 +67,7 @@ DAILY_DATA_QUERIES = {
               submission_date,
             FROM `moz-fx-data-shared-prod.telemetry.clients_daily`
             WHERE
-              submission_date = '{submission_date}'
+              submission_date BETWEEN '{start_date}' AND '{end_date}'
               AND attribution.content IS NOT NULL
           )
         INNER JOIN geo_t
@@ -313,7 +97,7 @@ DAILY_DATA_QUERIES = {
               submission_date,
             FROM `moz-fx-data-shared-prod.telemetry.clients_daily`
             WHERE
-              submission_date = '{submission_date}'
+              submission_date BETWEEN '{start_date}' AND '{end_date}'
               AND attribution.content IS NOT NULL
           )
         INNER JOIN geo_t
@@ -377,7 +161,7 @@ DAILY_DATA_QUERIES = {
               submission_date,
             FROM `moz-fx-data-shared-prod.telemetry.clients_daily`
             WHERE
-              submission_date = '{submission_date}'
+              submission_date BETWEEN '{start_date}' AND '{end_date}'
               AND attribution.content IS NOT NULL
           )
         INNER JOIN geo_t
@@ -408,7 +192,7 @@ DAILY_DATA_QUERIES = {
               submission_date,
             FROM `moz-fx-data-shared-prod.telemetry.clients_daily`
             WHERE
-              submission_date = '{submission_date}'
+              submission_date BETWEEN '{start_date}' AND '{end_date}'
               AND attribution.content IS NOT NULL
           )
         INNER JOIN geo_t
@@ -418,9 +202,9 @@ DAILY_DATA_QUERIES = {
 }
 
 
-def get_raw_data(bq_client, bq_storage_client, metric):
+def get_raw_data(bq_client, bq_storage_client, metric, start_date, end_date):
     return bq_client.query(
-        RAW_DATA_QUERIES[metric]
+        QUERIES[metric].format(start_date=start_date, end_date=end_date)
     ).result().to_dataframe(bqstorage_client=bq_storage_client)
 
 
@@ -466,5 +250,5 @@ def prepare_data(data):
 
 def get_data_for_date(bq_client, bq_storage_client, metric, model_date):
     return bq_client.query(
-        DAILY_DATA_QUERIES[metric].format(submission_date=model_date)
+        QUERIES[metric].format(start_date=model_date, end_date=model_date)
     ).result().to_dataframe(bqstorage_client=bq_storage_client)
